08ea22985a7d1f2b185941f268c614fd53ba4df0
Rodrigo freitas <foolha@gmail.com>
2017-04-20 19:18:27 -0300

    adicionando propriedades as classes Produtor e Propriedade
    - Em outra branch pretendo adicionar o lombok para ter das duas maneiras

--- src/main/java/br/com/codepampa/model/Produtor.java
+++ src/main/java/br/com/codepampa/model/Produtor.java
@@ -1,10 +1,73 @@
 package br.com.codepampa.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Produtor extends BaseEntity {
 
 
+    @Column(name = "nome", length = 125)
+    private String nome;
 
+    @Column(name = "inscricao_estadual", length = 20)
+    private String inscricaoEstadual;
+
+    @Column(name = "data_cadastro")
+    private LocalDate dataCadastro;
+
+    //Nesse caso aqui pode ser EAGER, pois um produtor nao vai ter muitaaaaas propiedades
+    //a ponto da performace cair
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Propriedade> propriedades = new HashSet<>();
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Produtor)) {
+            return false;
+        }
+
+        Produtor produtor = (Produtor) o;
+        return Objects.equals(this.inscricaoEstadual, produtor.getInscricaoEstadual());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(inscricaoEstadual);
+    }
 }
--- src/main/java/br/com/codepampa/model/Propriedade.java
+++ src/main/java/br/com/codepampa/model/Propriedade.java
@@ -1,7 +1,60 @@
 package br.com.codepampa.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import java.util.Objects;
 
 @Entity
 public class Propriedade extends BaseEntity {
+
+    @Column(name = "nome", length = 150)
+    private String nome;
+
+    @Column(name = "latitude", length = 80)
+    private String latitude;
+
+    @Column(name = "longitude", length = 80)
+    private String longitude;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    private String getLongitude() {
+        return longitude;
+    }
+
+    private void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Propriedade)) {
+            return false;
+        }
+        Propriedade that = (Propriedade) o;
+        return Objects.equals(this.latitude, that.getLatitude()) &&
+                Objects.equals(this.longitude, that.getLongitude());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude);
+    }
 }
